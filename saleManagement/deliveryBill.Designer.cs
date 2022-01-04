abac3b3e9f9143991f9543fa35278193fbbd7d8e
Dat Pham <phamtiendat.tdtu@gmail.com>
2022-01-05 03:00:43 +0700

    create delivery bill

--- saleManagement/deliveryBill.Designer.cs
+++ saleManagement/deliveryBill.Designer.cs
@@ -29,19 +29,300 @@ namespace saleManagement
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbIdDeliveryBill = new System.Windows.Forms.TextBox();
+            this.tbIdAccountant = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.createDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.saleManagementDataSet = new saleManagement.saleManagementDataSet();
+            this.ordersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ordersTableAdapter = new saleManagement.saleManagementDataSetTableAdapters.ordersTableAdapter();
+            this.idOrderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idCustomerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.paymentMethodDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.creationDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalPriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saleManagementDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Delivery Bill";
+            // 
+            // tbIdDeliveryBill
+            // 
+            this.tbIdDeliveryBill.Location = new System.Drawing.Point(158, 22);
+            this.tbIdDeliveryBill.Name = "tbIdDeliveryBill";
+            this.tbIdDeliveryBill.Size = new System.Drawing.Size(200, 22);
+            this.tbIdDeliveryBill.TabIndex = 1;
+            // 
+            // tbIdAccountant
+            // 
+            this.tbIdAccountant.Location = new System.Drawing.Point(158, 58);
+            this.tbIdAccountant.Name = "tbIdAccountant";
+            this.tbIdAccountant.Size = new System.Drawing.Size(200, 22);
+            this.tbIdAccountant.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID Accountant";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(158, 98);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ID Order";
+            // 
+            // createDate
+            // 
+            this.createDate.Location = new System.Drawing.Point(158, 143);
+            this.createDate.Name = "createDate";
+            this.createDate.Size = new System.Drawing.Size(200, 22);
+            this.createDate.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Creation Date";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(158, 186);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(141, 21);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "being transported";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Order status";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(158, 213);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 21);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "delivered";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 250);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 17);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Payment status";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(158, 250);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 21);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "paid";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(158, 278);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(72, 21);
+            this.radioButton4.TabIndex = 13;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "unpaid";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 46);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Create Delivery Bill";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idOrderDataGridViewTextBoxColumn,
+            this.idCustomerDataGridViewTextBoxColumn,
+            this.paymentMethodDataGridViewTextBoxColumn,
+            this.creationDateDataGridViewTextBoxColumn,
+            this.totalPriceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ordersBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(382, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(747, 416);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // saleManagementDataSet
+            // 
+            this.saleManagementDataSet.DataSetName = "saleManagementDataSet";
+            this.saleManagementDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ordersBindingSource
+            // 
+            this.ordersBindingSource.DataMember = "orders";
+            this.ordersBindingSource.DataSource = this.saleManagementDataSet;
+            // 
+            // ordersTableAdapter
+            // 
+            this.ordersTableAdapter.ClearBeforeFill = true;
+            // 
+            // idOrderDataGridViewTextBoxColumn
+            // 
+            this.idOrderDataGridViewTextBoxColumn.DataPropertyName = "idOrder";
+            this.idOrderDataGridViewTextBoxColumn.HeaderText = "idOrder";
+            this.idOrderDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idOrderDataGridViewTextBoxColumn.Name = "idOrderDataGridViewTextBoxColumn";
+            this.idOrderDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // idCustomerDataGridViewTextBoxColumn
+            // 
+            this.idCustomerDataGridViewTextBoxColumn.DataPropertyName = "idCustomer";
+            this.idCustomerDataGridViewTextBoxColumn.HeaderText = "idCustomer";
+            this.idCustomerDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idCustomerDataGridViewTextBoxColumn.Name = "idCustomerDataGridViewTextBoxColumn";
+            this.idCustomerDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // paymentMethodDataGridViewTextBoxColumn
+            // 
+            this.paymentMethodDataGridViewTextBoxColumn.DataPropertyName = "paymentMethod";
+            this.paymentMethodDataGridViewTextBoxColumn.HeaderText = "paymentMethod";
+            this.paymentMethodDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.paymentMethodDataGridViewTextBoxColumn.Name = "paymentMethodDataGridViewTextBoxColumn";
+            this.paymentMethodDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // creationDateDataGridViewTextBoxColumn
+            // 
+            this.creationDateDataGridViewTextBoxColumn.DataPropertyName = "creationDate";
+            this.creationDateDataGridViewTextBoxColumn.HeaderText = "creationDate";
+            this.creationDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.creationDateDataGridViewTextBoxColumn.Name = "creationDateDataGridViewTextBoxColumn";
+            this.creationDateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // totalPriceDataGridViewTextBoxColumn
+            // 
+            this.totalPriceDataGridViewTextBoxColumn.DataPropertyName = "totalPrice";
+            this.totalPriceDataGridViewTextBoxColumn.HeaderText = "totalPrice";
+            this.totalPriceDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.totalPriceDataGridViewTextBoxColumn.Name = "totalPriceDataGridViewTextBoxColumn";
+            this.totalPriceDataGridViewTextBoxColumn.Width = 125;
             // 
             // deliveryBill
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1141, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.createDate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbIdAccountant);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbIdDeliveryBill);
+            this.Controls.Add(this.label1);
             this.Name = "deliveryBill";
             this.Text = "Delivery Bill";
+            this.Load += new System.EventHandler(this.deliveryBill_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saleManagementDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ordersBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbIdDeliveryBill;
+        private System.Windows.Forms.TextBox tbIdAccountant;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker createDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private saleManagementDataSet saleManagementDataSet;
+        private System.Windows.Forms.BindingSource ordersBindingSource;
+        private saleManagementDataSetTableAdapters.ordersTableAdapter ordersTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idOrderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idCustomerDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn paymentMethodDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn creationDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totalPriceDataGridViewTextBoxColumn;
     }
 }
--- saleManagement/deliveryBill.cs
+++ saleManagement/deliveryBill.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,39 @@ namespace saleManagement
 {
     public partial class deliveryBill : Form
     {
+        SqlConnection con = new SqlConnection();
+        String strConn = ConfigurationManager.ConnectionStrings["dbconfig"].ConnectionString;
         public deliveryBill()
         {
             InitializeComponent();
+            con.ConnectionString = strConn;
+            updateTotalPrice();
+        }
+        private void updateTotalPrice()
+        {
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            string sql = "";
+
+            sql = "UPDATE o SET totalPrice = COALESCE(de.amount, 0) FROM orders o LEFT JOIN (select ord.idOrder, sum(price * quantity) as amount from detailOrder do, orders ord where ord.idOrder = do.idOrder group by ord.idOrder) de ON de.idOrder = o.idOrder;";
+
+            command = new SqlCommand(sql, con);
+
+            adapter.UpdateCommand = new SqlCommand(sql, con);
+            adapter.UpdateCommand.ExecuteNonQuery();
+
+            command.Connection.Close();
+            command.Dispose();
+            con.Close();
+        }
+
+        private void deliveryBill_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'saleManagementDataSet.orders' table. You can move, or remove it, as needed.
+            this.ordersTableAdapter.Fill(this.saleManagementDataSet.orders);
+
         }
     }
 }
