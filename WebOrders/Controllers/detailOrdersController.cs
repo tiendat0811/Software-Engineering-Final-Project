2eb1d87d76839a073220e972d83ec47ab61bcd4e
Dat Pham <phamtiendat.tdtu@gmail.com>
2022-01-05 02:21:00 +0700

    Add web order function

---
+++ WebOrders/Controllers/detailOrdersController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebOrders.Models;
+
+namespace WebOrders.Controllers
+{
+    public class detailOrdersController : Controller
+    {
+        private saleManagementEntities db = new saleManagementEntities();
+
+        // GET: detailOrders
+        public ActionResult Index()
+        {
+            var detailOrders = db.detailOrders.Include(d => d.item).Include(d => d.order);
+            return View(detailOrders.ToList());
+        }
+
+        // GET: detailOrders/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            detailOrder detailOrder = db.detailOrders.Find(id);
+            if (detailOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailOrder);
+        }
+
+        // GET: detailOrders/Create
+        public ActionResult Create()
+        {
+            ViewBag.idItem = new SelectList(db.items, "idItem", "nameItem");
+            ViewBag.idOrder = new SelectList(db.orders, "idOrder", "idCustomer");
+            return View();
+        }
+
+        // POST: detailOrders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idOrder,idItem,quantity,price")] detailOrder detailOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.detailOrders.Add(detailOrder);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idItem = new SelectList(db.items, "idItem", "nameItem", detailOrder.idItem);
+            ViewBag.idOrder = new SelectList(db.orders, "idOrder", "idCustomer", detailOrder.idOrder);
+            return View(detailOrder);
+        }
+
+        // GET: detailOrders/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            detailOrder detailOrder = db.detailOrders.Find(id);
+            if (detailOrder == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idItem = new SelectList(db.items, "idItem", "nameItem", detailOrder.idItem);
+            ViewBag.idOrder = new SelectList(db.orders, "idOrder", "idCustomer", detailOrder.idOrder);
+            return View(detailOrder);
+        }
+
+        // POST: detailOrders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idOrder,idItem,quantity,price")] detailOrder detailOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(detailOrder).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idItem = new SelectList(db.items, "idItem", "nameItem", detailOrder.idItem);
+            ViewBag.idOrder = new SelectList(db.orders, "idOrder", "idCustomer", detailOrder.idOrder);
+            return View(detailOrder);
+        }
+
+        // GET: detailOrders/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            detailOrder detailOrder = db.detailOrders.Find(id);
+            if (detailOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(detailOrder);
+        }
+
+        // POST: detailOrders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            detailOrder detailOrder = db.detailOrders.Find(id);
+            db.detailOrders.Remove(detailOrder);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebOrders/Controllers/ordersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebOrders.Models;
+
+namespace WebOrders.Controllers
+{
+    public class ordersController : Controller
+    {
+        private saleManagementEntities db = new saleManagementEntities();
+
+        // GET: orders
+        public ActionResult Index()
+        {
+            var orders = db.orders.Include(o => o.customer);
+            return View(orders.ToList());
+        }
+
+        // GET: orders/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.idCustomer = new SelectList(db.customers, "idCustomer", "nameCustomer");
+            return View();
+        }
+
+        // POST: orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idOrder,idCustomer,paymentMethod,creationDate,totalPrice")] order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idCustomer = new SelectList(db.customers, "idCustomer", "nameCustomer", order.idCustomer);
+            return View(order);
+        }
+
+        // GET: orders/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idCustomer = new SelectList(db.customers, "idCustomer", "nameCustomer", order.idCustomer);
+            return View(order);
+        }
+
+        // POST: orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idOrder,idCustomer,paymentMethod,creationDate,totalPrice")] order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idCustomer = new SelectList(db.customers, "idCustomer", "nameCustomer", order.idCustomer);
+            return View(order);
+        }
+
+        // GET: orders/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            order order = db.orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            order order = db.orders.Find(id);
+            db.orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
